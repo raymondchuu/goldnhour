67224c29feeade064714372b24175fe1e632b0fe
raymondchuu <raymondchu1996@gmail.com>
2020-11-23 22:19:41 -0500

    updated image size

--- src/components/HomePage/Displayimages.js
+++ src/components/HomePage/Displayimages.js
@@ -93,7 +93,7 @@ class DisplayImages extends React.Component {
                         if (url.media_url[8] == 'v') {
                             return (
                                 <div className="home-image">              
-                                    <video autoPlay muted loop style={{width: '25%', height: '25%'}}>
+                                    <video autoPlay muted loop style={{width: '100%', height: '100%'}}>
                                         <source src={url.media_url} type="video/mp4"/>
                                     </video>
                             </div>
